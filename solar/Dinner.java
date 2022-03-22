79f17a3e1167a597fccbbb017f771668a34a6df8
outofmap <enjoyhee@gmail.com>
2022-03-22 17:47:42 +0900

    어뷰징 히스토리 검증 추가
    마케팅팀 요청(ISSUE-323)으로 어뷰저 검증 정책 도입

--- solar/Dinner.java
+++ solar/Dinner.java
@@ -3,14 +3,26 @@ public class Dinner {
     private final DeliveryCenter deliveryCenter;
 
     public void orderDinner(User user, Order order) {
+        checkPreCondition(user);
         DinnerCart dinnerCart = orderFood();
         deliveryCenter.deliverOrder(DinnerCart dinnerCart, User user);
+    }
+
+    private void checkPreCondition(User user) {
+        checkUserCondition(user);
+    }
+
+    private void checkUserCondition(User user) {
+        private static int MAX_ABUSE_COUNT = 5;
+        if (user.getAbuseHistory().length > MAX_ABUSE_COUNT) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
     }
 
     private DinnerCart orderFood(Order order) {
         Food appitizer = new Appitizer(order);
         Food mainDish = new Main(order);
         Food dessert = new Dessert(order);
-        return DinnerCart.of(appitizer,mainDish,dessert);
+        return DinnerCart.of(appitizer, mainDish, dessert);
     }
 }
