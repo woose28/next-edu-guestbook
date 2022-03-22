1e18e61a219899e2718808e0b5dcd1a21b0a4a7e
outofmap <enjoyhee@gmail.com>
2022-03-22 18:51:01 +0900

    주문 배달 조건 검증 추가

--- solar/DeileveryCenter.java
+++ solar/DeileveryCenter.java
@@ -1,7 +1,27 @@
 @Service
 public class DeliveryCenter {
     public void deliverOrder(DinnerCart dinnerCart, User user) {
-        deliverOrder();
+        checkDeliveryCondition(dinnerCart, user);
+        CallResult callResult = callRiders();
+        if (callResult.isSuccess()) {
+            deliverOrder();
+        }
+    }
+
+    private void checkDeliveryCondition(DinnerCart dinnerCart, User user) {
+        if (dinnerCart.isOverWeight()) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
+        if (user.isAbuser()) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
+    }
+
+    private CallResult callRiders() {
+        if(Callcenter.getAnyRiders().hasRiders()) {
+            return CallResult.ofSuccess();
+        }
+        return CallResult.ofFail();
     }
 
     private void deliverOrder(DinnerCart dinnerCart, User user) {
--- solar/Dinner.java
+++ solar/Dinner.java
@@ -3,8 +3,27 @@ public class Dinner {
     private final DeliveryCenter deliveryCenter;
 
     public void orderDinner(User user, Order order) {
+        checkPreCondition(user, order);
         DinnerCart dinnerCart = orderFood();
         deliveryCenter.deliverOrder(DinnerCart dinnerCart, User user);
+    }
+
+    private void checkPreCondition(User user, Order order) {
+        checkUserCondition(user);
+        checkOrderCondition(order);
+    }
+
+    private void checkUserCondition(User user) {
+        private static int MAX_ABUSE_COUNT = 5;
+        if (user.getAbuseHistory().length > MAX_ABUSE_COUNT) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
+    }
+
+    private void checkOrderCondition(Order order) {
+        if (order.isInValidOrderTime() && order.isInValidPrice()) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
     }
 
     private DinnerCart orderFood(Order order) {
