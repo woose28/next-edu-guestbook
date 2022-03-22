81e79319c08c699e45da14c017c2f443598e5780
outofmap <enjoyhee@gmail.com>
2022-03-22 18:38:14 +0900

    check valid delivery condition
    #ISSUE-400 배달노동자 보호법 시행으로 배달가능 조건 검증 필요

--- solar/DeileveryCenter.java
+++ solar/DeileveryCenter.java
@@ -1,7 +1,17 @@
 @Service
 public class DeliveryCenter {
     public void deliverOrder(DinnerCart dinnerCart, User user) {
+        checkDeliveryCondition(dinnerCart, user);
         deliverOrder();
+    }
+
+    private void checkDeliveryCondition(DinnerCart dinnerCart, User user) {
+        if (dinnerCart.isOverWeight()) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
+        if (user.isAbuser()) {
+            throw new OrderException("주문이 불가능합니다.");
+        }
     }
 
     private void deliverOrder(DinnerCart dinnerCart, User user) {
